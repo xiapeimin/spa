8bc46dba2722a624a029bdcc43d1179034ed79d3
xiapeimin <2536733090@qq.com>
2020-04-02 17:49:04 +0800

    密码可见组件

--- 11-password-visual/main.js
+++ 11-password-visual/main.js
@@ -1,8 +1,15 @@
-$(function(){
-  //console.log('hello')
-  var $range = $('#range'),
-      $age   = $('#age');
-$range.change(function(){
- $age.html($range.val());
+$(function() {
+    var n = $("#pwd")
+      , o = $(".pwd-text")
+      , e = $("#eye");
+    n.on("input", function() {
+        o.val(n.val())
+    }),
+    e.mouseover(function() {
+        o.css("z-index", "10")
+    }),
+    e.mouseout(function() {
+        o.css("z-index", "-10")
+    })
 });
-})
+
