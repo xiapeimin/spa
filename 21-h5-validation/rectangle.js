5cbdc6b12b94b98dad15fb3fcb5940be02187513
xiapeimin <2536733090@qq.com>
2020-04-05 00:38:08 +0800

    auto

--- 21-h5-validation/rectangle.js
+++ 21-h5-validation/rectangle.js
@@ -5,14 +5,17 @@ $(function() {
       $form = $('form');
       $perimeter = $('#perimeter'),
       $area = $('#area');
+  function i(e, t) {
+    return Math.round(e * Math.pow(10, t)) / Math.pow(10, t);
+  } 
 
   $form.submit(function(e){
     e.preventDefault();
     var w = Number($width.val()),
         h = Number($height.val());
 
-    var p = 2 * (w + h),
-        a = w * h;
+    var p = i(2 * (w + h),2),
+        a = i(w * h,2);
 
     $perimeter.val(p);
     $area.val(a);
